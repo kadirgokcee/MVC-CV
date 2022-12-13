1c795cad0d2e304dd84c54c1db88e39f0a5953e2
gokce <gokce@KADRGOKCE>
2022-12-13 18:06:17 +0300

    Add project files.

---
+++ MvcCv/Controllers/HobiController.cs
@@ -0,0 +1,33 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class HobiController : Controller
+    {
+        // GET: Hobi
+        GenericRepository<TblHobilerim> repo = new GenericRepository<TblHobilerim>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var hobiler = repo.List();
+            return View(hobiler);
+        }
+        [HttpPost]
+        public ActionResult Index(TblHobilerim p)
+        {
+            var t = repo.Find(x => x.ID == 1);
+            t.Aciklama1 = p.Aciklama1;
+            t.Aciklama2 = p.Aciklama2;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ MvcCv/Controllers/SosyalMedyaController.cs
@@ -0,0 +1,57 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class SosyalMedyaController : Controller
+    {
+        // GET: SosyalMedya
+        GenericRepository<TblSosyalMedya> repo = new GenericRepository<TblSosyalMedya>();
+        public ActionResult Index()
+        {
+            var veriler = repo.List();
+            return View(veriler);
+        }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(TblSosyalMedya p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult SayfaGetir(int id)
+        {
+            var hesap = repo.Find(x=> x.ID== id);
+            return View(hesap);
+        }
+        [HttpPost]
+        public ActionResult SayfaGetir(TblSosyalMedya p)
+        {
+            var hesap = repo.Find(x => x.ID == p.ID);
+            hesap.Durum = true;
+            hesap.Ad = p.Ad;
+            hesap.Link = p.Link;
+            hesap.ikon = p.ikon;
+            repo.TUpdate(hesap);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var hesap = repo.Find(x => x.ID == id);
+            hesap.Durum = false;
+            repo.TUpdate(hesap);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
